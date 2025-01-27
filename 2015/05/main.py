66b4fe202c60f680223d17bb7c33e95f16740be3
W. S. Wellington <thewellington@gmail.com>
2025-01-27 14:29:23 -0800

    working through some details

--- 2015/05/main.py
+++ 2015/05/main.py
@@ -8,26 +8,55 @@ __author__ = "thewellington@gmail.com"
 __version__ = ""
 __license__ = ""
 
-
+import re
 
 vowels = "AaEeIiOoUu"
-forbidden = ["ab", "cd", "pq", "xy"]
-    
+forbidden_strings = ["ab", "cd", "pq", "xy"]
+
+def old_vowel_count(): 
+    for string in data:
+        get_vowels = [each for each in string if each in vowels]
+        if len(get_vowels) >= 3:
+            print(len(get_vowels))
+            return true
+
+def vowel_count(string):
+    get_vowels = [each for each in string if each in vowels]
+    result = bool(len(get_vowels) >= 3)
+    return result
+
+def find_double_chars(string):
+    result = bool(re.search(r"(.)\1", string))
+    return result
+
+def find_forbidden(string):
+    for f in forbidden_strings:
+        forbidden = string.find(f)
+        if forbidden == -1:
+            return False
+        else:
+            return True
+
 def main():
     """ Main entry point of the app """
 
-with open('input.txt', 'r') as file:
-    data = file.readlines()
-    
-for string in data:
-    get_vowels = [each for each in string if each in vowels]
-    if len(get_vowels) >= 3:
-        print(len(get_vowels))
-    
-    
+    string_counter = 0
+
+    with open('input.txt', 'r') as file:
+        data = file.readlines()
+ 
+    for line in data:
+        print(line)
+        if find_forbidden(line) == True:
+            print("forbidden")
+        
+        if find_double_chars(line) == True and vowel_count(line) == True:
+            string_counter += 1
+       
+    print(string_counter)
+
 if __name__ == "__main__":
     """ This is executed when run from the command line """
     main()
 
 
-""" still need to take care of second and third cases """
