00fdbc27c9cb1c6e8a98e01fc4db85d592c2db6e
W. S. Wellington <thewellington@gmail.com>
2025-02-04 16:59:01 -0800

    Completing day 5

--- 2015/05/main.py
+++ 2015/05/main.py
@@ -13,13 +13,6 @@ import re
 vowels = "AaEeIiOoUu"
 forbidden_strings = ["ab", "cd", "pq", "xy"]
 
-def old_vowel_count(): 
-    for string in data:
-        get_vowels = [each for each in string if each in vowels]
-        if len(get_vowels) >= 3:
-            print(len(get_vowels))
-            return true
-
 def vowel_count(string):
     get_vowels = [each for each in string if each in vowels]
     result = bool(len(get_vowels) >= 3)
@@ -37,23 +30,48 @@ def find_forbidden(string):
         else:
             return True
 
-def main():
-    """ Main entry point of the app """
+def find_letter_pairs(string):
+    result =  bool(re.search(r"(..).*\1", string))
+    return result
 
+def skip_repeat(string):
+    result = bool(re.search(r"(.).\1", string))
+    return result
+
+
+
+def part_1(data):
     string_counter = 0
 
-    with open('input.txt', 'r') as file:
-        data = file.readlines()
- 
     for line in data:
-        print(line)
-        if find_forbidden(line) == True:
-            print("forbidden")
-        
+       
         if find_double_chars(line) == True and vowel_count(line) == True:
             string_counter += 1
-       
-    print(string_counter)
+    
+        if find_forbidden(line) == True:
+            string_counter -= 1 
+
+    print("Part 1 - " + str(string_counter) + " nice strings.")
+
+def part_2(data):
+    string_counter_2 = 0
+
+    for line in data:
+    
+        if find_letter_pairs(line) == True and skip_repeat(line) == True:
+            string_counter_2 += 1
+
+    print("Part 2 - " + str(string_counter_2) + " nice strings.")
+
+def main():
+    """ Main entry point of the app """
+    
+    with open('input.txt', 'r') as file:
+        data = file.readlines()
+
+    part_1(data)
+
+    part_2(data)
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
